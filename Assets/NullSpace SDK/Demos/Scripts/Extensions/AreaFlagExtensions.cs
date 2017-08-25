5213fbd9f998620fd29ac336441a562b81f7166f
Jonathan Palmer (Laptop) <vantharion@gmail.com>
2017-08-24 17:51:00 -0700

    Added Mirror Function.

---
+++ Assets/NullSpace SDK/Demos/Scripts/Extensions/AreaFlagExtensions.cs
@@ -0,0 +1,18 @@
+﻿
+namespace NullSpace.SDK
+{
+	using System;
+	using UnityEngine;
+
+	public static class AreaFlagExtensions
+	{
+		public static AreaFlag Mirror(this AreaFlag lhs)
+		{
+			return (AreaFlag)RotateLeft((uint)lhs, 16);
+		}
+		private static uint RotateLeft(uint x, byte n)
+		{
+			return ((x << n) | (x >> (32 - n)));
+		}
+	}
+}
